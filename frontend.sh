c5e00973a43af164204b7b152b8f16229bdc97a1
rajesh <rajeshkaramsetty1999@gmail.com>
2025-11-07 22:28:25 +0530

    adding mongodb service

--- frontend.sh
+++ frontend.sh
@@ -35,15 +35,15 @@ VALIDATE(){
 
 
 
-dnf module disable nginx -y
+dnf module disable nginx -y &>>$LOG_FILE
 VALIDATE $? "disabling nginx version"
 
 
-dnf module enable nginx:1.24 -y
+dnf module enable nginx:1.24 -y &>>$LOG_FILE
 VALIDATE $? "enabling the nginx version"
 
 
-dnf install nginx -y
+dnf install nginx -y &>>$LOG_FILE
 VALIDATE $? "installing the nginx"
 
 
@@ -55,13 +55,13 @@ VALIDATE $? "enabling and starting the nginx"
 rm -rf /usr/share/nginx/html/* 
 VALIDATE $? "removing default index.html content"
 
-curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend-v3.zip
+curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend-v3.zip &>>$LOG_FILE
 VALIDATE $? "downloading nginx zip file"
 
 cd /usr/share/nginx/html 
 VALIDATE $? "changing directory to html folder"
 
-unzip /tmp/frontend.zip
+unzip /tmp/frontend.zip &>>$LOG_FILE
 VALIDATE $? "unzipping the nginx zip file"
 
 
@@ -69,7 +69,7 @@ cp $SCRIPT_DIR/nginx.conf /etc/nginx/nginx.conf
 VALIDATE $? "copying the our nginx conf file"
 
 
-systemctl restart nginx 
+systemctl restart nginx &>>$LOG_FILE
 VALIDATE $? "Restating the nginx"
 
 
