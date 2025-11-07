4a3e6a58357369a5aa4140ad1cbf2297acb08380
rajesh <rajeshkaramsetty1999@gmail.com>
2025-11-07 22:12:21 +0530

    adding mongodb service

--- catalogue.sh
+++ catalogue.sh
@@ -63,6 +63,8 @@ VALIDATE $? "creating app directory"
 curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue-v3.zip &>>$LOG_FILE
 VALIDATE $? "downloading the catalogue zip file"
 
+
+rm -rf /app/*
 cd /app 
 unzip /tmp/catalogue.zip &>>$LOG_FILE
 VALIDATE $? "unzipping the zip file"
